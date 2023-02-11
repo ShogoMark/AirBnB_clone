16911cb244763c992b3aae8e6f18ce30451f9926
ShogoMark <trigmark87@gmail.com>
2023-02-11 15:48:28 +0100

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 import uuid
 from datetime import datetime
 
@@ -35,31 +34,5 @@ class BaseModel():
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
-=======
-""" base model module """
-
-import uuid
-import datetime from datetime
-
-
-class BaseModel:
-    """ defines a class BaseModel """
-
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = self.__class__.__name__
-        dict_copy["created_at"] = self.created_at.isoforma()
->>>>>>> 1231c91d86ed7af969029b5d450adec54186f0bf
         dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
