e25c4b53f7db9d66aeee5abda8eceb4caeb47f36
igraziella <igraziella2@gmail.com>
2023-02-12 00:15:44 -0800

    checking with pycode

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
-from models.state imoprt State
+from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
