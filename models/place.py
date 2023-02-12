c70434d35d083aec7551b63912674c8967e00fb1
igraziella <igraziella2@gmail.com>
2023-02-12 06:58:46 -0800

    fixing bugs

--- models/place.py
+++ models/place.py
@@ -5,9 +5,9 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ Defines a class - Place """
-    city_id: = ""
-    user_id: = ""
-    name: = ""
+    city_id = ""
+    user_id = ""
+    name = ""
     description = ""
     number_rooms = 0
     number_bathrooms = 0
